9cbe193ba6756bf51421833791d5037b3b461d7e
Steven Than <steventhan11@gmail.com>
2018-04-28 16:58:37 -0400

    start working on client

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    )
+  }
+}
+
+
+export default Home;
