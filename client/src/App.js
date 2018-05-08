4833ba803a075b0fb63e9bc06984d2c249c019a1
Steven Than <steventhan11@gmail.com>
2018-05-08 22:14:53 +0700

    added proper routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CssBaseline } from "material-ui";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+
+import NavBar from "./components/menus/NavBar";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    common: {
+      white: "#fff",
+      black: "#000"
+    },
+    primary: {
+      main: "#4d5668",
+      contrastText: "#fafafa"
+    },
+    secondary: {
+      main: "#6480A8",
+      contrastText: "#fafafa"
+    },
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <MuiThemeProvider theme={ theme }>
+        <Router>
+          <Fragment>
+            <CssBaseline />
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={ Home }/>
+              <Route exact path="/projects" component={ Projects }/>
+            </Switch>
+          </Fragment>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
