05ad8f7cbd87ae55e93f0f2219b9734936858bb7
Steven Than <steventhan11@gmail.com>
2018-05-08 22:04:45 +0700

    ContactOptions component

---
+++ client/src/components/ContactOptions.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { withStyles, Button } from "material-ui";
+
+const options = [
+  { label: "email", url: "mailto:steven@thanst.com" },
+  { label: "github", url: "https://github.com/steventhan" },
+  { label: "linkedin", url: "https://linkedin.com/in/steven-than" },
+]
+
+const styles = {
+  root: {
+    display: "flex",
+    justifyContent: "space-around",
+    padding: 20
+  },
+}
+
+class ContactOptions extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={ classes.root }>
+        { options.map((option, i) => (
+          <Button size="small" color="secondary" key={i} href={ option.url }> { option.label } </Button>)
+        )}
+      </div>
+    );
+  }
+}
+
+
+export default withStyles(styles)(ContactOptions);
