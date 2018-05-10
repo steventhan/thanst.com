cd5eb657ee45d217f6031bf5d09a89cc48dd12ce
Steven Than <steventhan11@gmail.com>
2018-05-10 15:08:42 +0700

    updated Projects component

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,7 +1,10 @@
-import React, { Component } from "react";
-import { Avatar, withStyles } from "material-ui";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { Avatar, withStyles, Grid, Typography, TextField } from "material-ui";
 
-import ContactOptions from "./ContactOptions";
+import SearchBoxContainer from "../containers/SearchBoxContainer";
+import ProjectListContainer from "../containers/ProjectListContainer";
+import projects from "../fake-data";
 import avatar from "../avatar.jpg";
 
 
@@ -11,17 +14,35 @@ const styles = {
     flexDirection: "column",
     width: "100%",
     height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingBottom: 100,
+    paddingTop: 60,
   },
+  section: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  }
 }
+
 
 class Projects extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={ classes.root }>
+      <div className={classes.root}>
+        <Grid className={classes.section} container spacing={0} justify="center" >
+          <Grid item xs={12} >
+            <Typography align="center" variant="title">Projects</Typography>
+          </Grid>
+        </Grid>
+        <Grid className={classes.section} container spacing={0} justify="flex-end">
+          <Grid item xs={12} md={4} lg={2}>
+            <SearchBoxContainer />
+          </Grid>
+        </Grid>
+        <Grid className={classes.section} container spacing={0} >
+          <Grid item xs={12} >
+            <ProjectListContainer />
+          </Grid>
+        </Grid>
       </div>
     );
   }
