650ee16059a6397a44ca91b5f648b64246b5bf22
Steven Than <steventhan11@gmail.com>
2018-05-10 15:01:13 +0700

    added more keys to fake data

---
+++ client/src/fake-data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    title: "My HNG",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["inteventory", "Flask", "MySQL", "jQuery"]
+  },
+  {
+    id: 2,
+    title: "Seattle Sporting Parks",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["park", "sport", "Seattle", "javascript", "heroku"]
+  },
+  {
+    id: 3,
+    title: "Deal Thief",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["travel", "deal", "Pyramid", "Python", "PostgreSQL"]
+  },
+  {
+    id: 4,
+    title: "Secured Pi",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["vision", "facial regconition", "Django", "Flask", "socketio"]
+  },
+  {
+    id: 5,
+    title: "Smart Marino",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["virtual queue", "qr", "NodeJS", "ReactJS", "Material-UI"]
+  },
+  {
+    id: 6,
+    title: "Dota 2 Bot",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
+    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
+  },
+];
