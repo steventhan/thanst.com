016c08de88e5e35f37be7d90ba6d69c568ddb7ef
Steven Than <steventhan11@gmail.com>
2018-05-10 15:09:41 +0700

    implemented nav buttons

--- client/src/components/menus/NavBar.js
+++ client/src/components/menus/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import { AppBar, Button, Toolbar } from "material-ui";
 import { withStyles } from "material-ui/styles";
 
@@ -31,11 +32,20 @@ class NavBar extends Component {
   render() {
     const classes = this.props;
     return (
-      <AppBar style={ styles.appbar }>
-        <Toolbar style={ styles.toolbar }>
-          { buttons.map((button, i) => {
-             return <Button size="small" color="secondary" variant="raised">{ button.label }</Button>;
-          })}
+      <AppBar style={styles.appbar}>
+        <Toolbar style={styles.toolbar}>
+          {buttons.map((button, i) => (
+              <Button
+                component={NavLink}
+                to={button.path}
+                size="small"
+                color="secondary"
+                variant="raised"
+              >
+                {button.label}
+              </Button>
+            )
+          )}
         </Toolbar>
       </AppBar>
     );
