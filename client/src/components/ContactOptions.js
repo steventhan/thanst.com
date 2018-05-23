0275bd54aa5401801952705ba1f0064cb4522875
Steven Than <steventhan11@gmail.com>
2018-05-23 18:59:12 +0700

    added icons for contact options

--- client/src/components/ContactOptions.js
+++ client/src/components/ContactOptions.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { withStyles, Button } from "material-ui";
+import { withStyles, Button, Icon } from "@material-ui/core";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from "@fortawesome/fontawesome-free-brands";
+import { AlarmOff } from '@material-ui/icons';
+import { EmailOutline, LinkedinBox, GithubCircle } from "mdi-material-ui";
+
+
+const iconStyle = {
+  marginRight: 3,
+  fontSize: 20
+}
 
 const options = [
-  { label: "email", url: "mailto:steven@thanst.com" },
-  { label: "github", url: "https://github.com/steventhan" },
-  { label: "linkedin", url: "https://linkedin.com/in/steven-than" },
+  { label: "email", icon: <EmailOutline style={iconStyle} />,  url: "mailto:steven@thanst.com" },
+  { label: "github", icon: <GithubCircle style={iconStyle} />, url: "https://github.com/steventhan" },
+  { label: "linkedin", icon: <LinkedinBox style={iconStyle} />, url: "https://linkedin.com/in/steven-than" },
 ];
 
 const styles = {
@@ -18,7 +28,8 @@ const styles = {
 const ContactOptions = ({ classes }) => (
   <div className={classes.root}>
     {options.map((option, i) => (
-      <Button size="small" color="secondary" key={i} href={option.url}>
+      <Button title={option.label} size="small" color="secondary" key={i} href={option.url}>
+        {option.icon}
         {option.label}
       </Button>)
     )}
