9a6c97db7a13e5b491c867a90c9a5973972a9a1e
Steven Than <steventhan11@gmail.com>
2018-05-23 19:02:11 +0700

    moved filtering to a helper function

--- client/src/containers/ProjectListContainer.js
+++ client/src/containers/ProjectListContainer.js
@@ -4,8 +4,28 @@ import { connect } from "react-redux";
 import ProjectList from "../components/ProjectList";
 import projects from "../fake-data";
 
-const mapStateToProps = state => ({
-  projects: projects
+const filterProjects =  (projects, searchText) => {
+  if (searchText.length === 0) return projects;
+
+  const searchWords = searchText.toLowerCase().split(",").reduce((wordList, word) => {
+    let trimmed = word.trim();
+    if (trimmed.length > 0) {
+      wordList.push(trimmed);
+    }
+    return wordList;
+  }, []);
+
+  return projects.filter(project => {
+    return searchWords.some(word => {
+      return project.title.toLowerCase().includes(word)
+              || project.intro.toLowerCase().includes(word)
+              || project.tags.some(tag => tag.toLowerCase().includes(word));
+    });
+  })
+}
+
+const mapStateToProps = store => ({
+  projects: filterProjects(projects, store.searchText)
 });
 
 export default connect(mapStateToProps)(ProjectList);
