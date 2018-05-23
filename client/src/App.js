b3ed0eaca7881d5ec4e5de6dfe7505bae04239c3
Steven Than <steventhan11@gmail.com>
2018-05-23 18:57:17 +0700

    moved theme out of App component, added ScrollBars for panes as well as other custom props

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,20 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-import { CssBaseline } from "material-ui";
+import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
 import SplitPane from "react-split-pane";
+import { Scrollbars } from "react-custom-scrollbars";
 import $ from "jquery";
 
+import theme, { primary, secondary } from "./theme";
 import { resizePane } from "./actions/splitPaneActions";
 import NavBar from "./components/menus/NavBar";
+import ResumeFloatingButton from "./components/menus/ResumeFloatingButton";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 
-const theme = createMuiTheme({
-  palette: {
-    type: "dark",
-    common: {
-      white: "#fff",
-      black: "#000"
-    },
-    primary: {
-      main: "#4d5668",
-      contrastText: "#fafafa"
-    },
-    secondary: {
-      main: "#6480A8",
-      contrastText: "#fafafa"
-    },
-  }
-});
 
 const mapStateToProps = store => ({ splitPane: store.splitPane });
 
@@ -35,23 +22,30 @@ class App extends Component {
   handlePaneResize = width => this.props.dispatch(resizePane(width));
 
   render() {
+    const { splitPane } = this.props;
     return (
       <MuiThemeProvider theme={theme}>
         <Router>
           <div>
             <CssBaseline />
             <NavBar />
+            <ResumeFloatingButton />
             <SplitPane
               split="vertical"
               onChange={this.handlePaneResize}
-              size={this.props.splitPane}
-              resizerStyle={{ width: 10, backgroundColor: "red" }}
+              minSize={300}
+              size={splitPane.open ? splitPane.size : "100%"}
+              resizerStyle={{ width: 7, backgroundColor: secondary }}
             >
-              <Switch>
-                <Route exact path="/" component={AboutMe}/>
-                <Route exact path="/projects" component={Projects}/>
-              </Switch>
-              <div></div>
+              <Scrollbars>
+                <Switch>
+                  <Route exact path="/" component={AboutMe}/>
+                  <Route exact path="/projects" component={Projects}/>
+                </Switch>
+              </Scrollbars>
+              <Scrollbars>
+                <Resume />
+              </Scrollbars>
             </SplitPane>
           </div>
         </Router>
