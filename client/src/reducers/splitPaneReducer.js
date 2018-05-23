4685f253b8c5e5d3aa0206ddf870c4cbf633653b
Steven Than <steventhan11@gmail.com>
2018-05-23 18:50:43 +0700

    added TOGGLE action for splitPane

--- client/src/reducers/splitPaneReducer.js
+++ client/src/reducers/splitPaneReducer.js
@@ -1,11 +1,23 @@
 import $ from "jquery";
 
-const initialState = parseInt($(window).width()/2, 10);
+const initialState = {
+  open: false,
+  size: parseInt($(window).width()/2, 10),
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "RESIZE":
-      return action.width;
+      return {
+        ...state,
+        size: action.width,
+      };
+
+    case "TOGGLE":
+      return {
+        ...state,
+        open: !state.open,
+      };
 
     default:
       return state;
