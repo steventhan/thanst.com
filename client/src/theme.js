ac8a904b8c3b7c5c84586d4b29110fb7992f77d1
Steven Than <steventhan11@gmail.com>
2018-05-23 18:49:15 +0700

    moved theme settings to an independent file

---
+++ client/src/theme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from "material-ui/styles";
+
+export const primary ="#4d5668";
+export const secondary = "#6480A8";
+
+export default createMuiTheme({
+  palette: {
+    type: "dark",
+    common: {
+      white: "#fff",
+      black: "#000"
+    },
+    primary: {
+      main: primary,
+      contrastText: "#fafafa"
+    },
+    secondary: {
+      main: secondary,
+      contrastText: "#fafafa"
+    },
+  }
+});
