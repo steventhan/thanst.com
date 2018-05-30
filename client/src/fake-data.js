86352c3882e6c7b08dc76f853b40989c33b85ea5
Steven Than <steventhan11@gmail.com>
2018-05-30 13:26:32 +0700

    removed duplicated fake data

--- client/src/fake-data.js
+++ client/src/fake-data.js
@@ -3,108 +3,42 @@ export default [
     id: 1,
     title: "My HNG",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["inteventory", "Flask", "MySQL", "jQuery"]
+    tags: ["inteventory", "Flask", "MySQL", "jQuery"],
+    slug: "my-hng"
   },
   {
     id: 2,
     title: "Seattle Sporting Parks",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["park", "sport", "Seattle", "javascript", "heroku"]
+    tags: ["park", "sport", "Seattle", "javascript", "heroku"],
+    slug: "seattle-field-finder"
   },
   {
     id: 3,
     title: "Deal Thief",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["travel", "deal", "Pyramid", "Python", "PostgreSQL"]
+    tags: ["travel", "deal", "Pyramid", "Python", "PostgreSQL"],
+    slug: "deal-thief"
   },
   {
     id: 4,
     title: "Secured Pi",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["vision", "facial regconition", "Django", "Flask", "socketio"]
+    tags: ["vision", "facial regconition", "Django", "Flask", "socketio"],
+    slug: "secured-pi"
   },
   {
     id: 5,
     title: "Smart Marino",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["virtual queue", "qr", "NodeJS", "ReactJS", "@material-ui/core"]
+    tags: ["virtual queue", "qr", "NodeJS", "ReactJS", "@material-ui/core"],
+    slug: "smart-marino"
   },
   {
     id: 6,
     title: "Dota 2 Bot",
     intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
+    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch"],
+    slug: "dota-2-bot"
   },
-  {
-    id: 7,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 8,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 9,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 10,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 11,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 12,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 13,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 14,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 15,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 16,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 17,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-  {
-    id: 18,
-    title: "Dota 2 Bot",
-    intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed dui non sapien vulputate dapibus id et velit. Nullam lacinia sapien sit amet nunc facilisis",
-    tags: ["machine learning", "network", "deep q", "neural", "Japronto", "PyTorch", ]
-  },
-];
+]
