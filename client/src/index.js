3b429264a7d9e77d7f93738790debf9f76c2cf5e
Steven Than <steventhan11@gmail.com>
2018-06-04 12:52:03 +0700

    brought reduxStore initialization to a separate file

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-import reducers from "./reducers";
+import store from "./reduxStore";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById('root'));
