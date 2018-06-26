4d34ccaa59b14cedccea2fa2d46d18de69ac75ce
Steven Than <steventhan11@gmail.com>
2018-06-26 20:31:33 +0700

    implemented react-snapshot

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import { render } from "react-snapshot";
 
 import store from "./reduxStore";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>
-  , document.getElementById('root'));
+  , document.getElementById('root')
+);
 registerServiceWorker();
